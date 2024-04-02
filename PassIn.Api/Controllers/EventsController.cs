0c916d892effa2b943038353e888bf5760cf5d66
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-04-01 22:25:37 -0300

    feat: start project

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+
+namespace PassIn.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestEventJson request)
+    {
+        var useCase = new RegisterEventUseCase();
+
+        useCase.Execute();
+
+        return Created();
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,10 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register;
+public class RegisterEventUseCase
+{
+    public void Execute(RequestEventJson request)
+    {
+
+    }
+}
