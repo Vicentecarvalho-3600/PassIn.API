86d4851ae9ac8d023369aede5bb067965feb95e1
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-04-02 15:26:48 -0300

    feat: add PassInDbContext and Event

---
+++ PassIn.Infrastructure/Entities/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace PassIn.Infrastructure.Entities;
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public string Maximun_Attendees { get; set; } = string.Empty;
+ 
+}
---
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PassIn.Infrastructure;
+internal class PassInDbContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\db\\PassInDb.db");
+    }
+}
